a2968c18b91801c693a6fac16d21d2f5f423bc0c
Dillerart <Dillerart69@gmail.com>
2015-09-26 16:16:23 +0500

    Начало проекта.

---
+++ Main.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Silver
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Клиенты");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Неисправности");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Оборудование");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Предприятие     ", new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6,
+            treeNode7});
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиСоединияСБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.импортНеисправностейToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.импортОборудованияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выгрузкаДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справочникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.контрагентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.неисправностиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.категорииОборудованияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отчетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBoxInform = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView.Enabled = false;
+            this.treeView.Location = new System.Drawing.Point(6, 11);
+            this.treeView.Name = "treeView";
+            treeNode5.Name = "clientTree";
+            treeNode5.NodeFont = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic);
+            treeNode5.Text = "Клиенты";
+            treeNode6.Name = "brakeTree";
+            treeNode6.NodeFont = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic);
+            treeNode6.Text = "Неисправности";
+            treeNode7.Name = "rigTree";
+            treeNode7.NodeFont = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic);
+            treeNode7.Text = "Оборудование";
+            treeNode8.Name = "MainTree";
+            treeNode8.NodeFont = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            treeNode8.Text = "Предприятие     ";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode8});
+            this.treeView.Size = new System.Drawing.Size(229, 491);
+            this.treeView.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.справочникиToolStripMenuItem,
+            this.отчетToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1136, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиСоединияСБДToolStripMenuItem,
+            this.импортНеисправностейToolStripMenuItem,
+            this.импортОборудованияToolStripMenuItem,
+            this.выгрузкаДанныхToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // настройкиСоединияСБДToolStripMenuItem
+            // 
+            this.настройкиСоединияСБДToolStripMenuItem.Name = "настройкиСоединияСБДToolStripMenuItem";
+            this.настройкиСоединияСБДToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
+            this.настройкиСоединияСБДToolStripMenuItem.Text = "Настройки соединия с БД";
+            // 
+            // импортНеисправностейToolStripMenuItem
+            // 
+            this.импортНеисправностейToolStripMenuItem.Name = "импортНеисправностейToolStripMenuItem";
+            this.импортНеисправностейToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
+            this.импортНеисправностейToolStripMenuItem.Text = "Импорт неисправностей";
+            // 
+            // импортОборудованияToolStripMenuItem
+            // 
+            this.импортОборудованияToolStripMenuItem.Name = "импортОборудованияToolStripMenuItem";
+            this.импортОборудованияToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
+            this.импортОборудованияToolStripMenuItem.Text = "Импорт оборудования";
+            // 
+            // выгрузкаДанныхToolStripMenuItem
+            // 
+            this.выгрузкаДанныхToolStripMenuItem.Name = "выгрузкаДанныхToolStripMenuItem";
+            this.выгрузкаДанныхToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
+            this.выгрузкаДанныхToolStripMenuItem.Text = "Выгрузка данных";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // справочникиToolStripMenuItem
+            // 
+            this.справочникиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.контрагентыToolStripMenuItem,
+            this.неисправностиToolStripMenuItem,
+            this.категорииОборудованияToolStripMenuItem});
+            this.справочникиToolStripMenuItem.Name = "справочникиToolStripMenuItem";
+            this.справочникиToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.справочникиToolStripMenuItem.Text = "Справочники";
+            // 
+            // контрагентыToolStripMenuItem
+            // 
+            this.контрагентыToolStripMenuItem.Name = "контрагентыToolStripMenuItem";
+            this.контрагентыToolStripMenuItem.Size = new System.Drawing.Size(242, 22);
+            this.контрагентыToolStripMenuItem.Text = "Контрагенты";
+            // 
+            // неисправностиToolStripMenuItem
+            // 
+            this.неисправностиToolStripMenuItem.Name = "неисправностиToolStripMenuItem";
+            this.неисправностиToolStripMenuItem.Size = new System.Drawing.Size(242, 22);
+            this.неисправностиToolStripMenuItem.Text = "Неисправности оборудования";
+            // 
+            // категорииОборудованияToolStripMenuItem
+            // 
+            this.категорииОборудованияToolStripMenuItem.Name = "категорииОборудованияToolStripMenuItem";
+            this.категорииОборудованияToolStripMenuItem.Size = new System.Drawing.Size(242, 22);
+            this.категорииОборудованияToolStripMenuItem.Text = "Категории оборудования";
+            // 
+            // отчетToolStripMenuItem
+            // 
+            this.отчетToolStripMenuItem.Name = "отчетToolStripMenuItem";
+            this.отчетToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.отчетToolStripMenuItem.Text = "Отчет";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.treeView);
+            this.groupBox1.Location = new System.Drawing.Point(2, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1129, 512);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(241, 11);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(882, 491);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textBoxInform});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 544);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1136, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBoxInform
+            // 
+            this.textBoxInform.Name = "textBoxInform";
+            this.textBoxInform.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 566);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Silver";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиСоединияСБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem импортНеисправностейToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem импортОборудованияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выгрузкаДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справочникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem контрагентыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem неисправностиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem категорииОборудованияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отчетToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel textBoxInform;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Silver
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            informer(true);
+        }
+
+        private void informer(bool info)
+        {
+           if(info){
+               textBoxInform.ForeColor = System.Drawing.Color.Green;
+               textBoxInform.Text = "Состояние подключения к БД: Покдлючено.";
+               treeView.Enabled = true;
+           }
+           else
+           {
+               textBoxInform.ForeColor = System.Drawing.Color.Red;
+               textBoxInform.Text = "Состояние подключения к БД: Отсутствует.";
+               treeView.Enabled = false;
+           }
+           
+        }
+
+    }
+}
